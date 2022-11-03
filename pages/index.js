8fa4f2a2241be5865746de6cb151a001a36df625
Kimo <48866163+NotReem@users.noreply.github.com>
2022-11-03 17:14:09 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -17,8 +17,6 @@ export default function Home() {
           <iframe src="https://game316009.konggames.com/gamez/0031/6009/live/index.html" width="350%" height="500" class="wp-embed-frame " frameborder="0"></iframe>
           Slope :D
           <iframe id="iframehtml5" width="100%" height="100%" src="https://kdata1.com/2020/05/slope/" frameborder="0" border="0" scrolling="no" class="iframe-default " allowfullscreen=""></iframe>
-          Geometry Dash :>
-          <iframe src="https://scratch.mit.edu/projects/207455066/embed" name="iFrame Name" scrolling="No" height="500px" width="100%" style="border: none;"></iframe>ups allow-presentation allow-scripts allow-same-origin" style="border: 0px; background-color: rgb(255, 255, 255); width: 10px; height: 10px; min-width: 100%; min-height: 100%;"></iframe>
         </p>
       </main>
 
